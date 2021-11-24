4fad79f6f494ad455c5d0aef86ff51f7a1704456
Sam Klepper <samklepdev@gmail.com>
2021-11-24 11:08:08 -0600

    Extending colors module.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
