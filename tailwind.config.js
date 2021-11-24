0162810ad2c771b7047ce1c2090da6e2d0ab892d
Sam Klepper <samklepdev@gmail.com>
2021-11-24 11:28:20 -0600

    Removed color config from tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
-const { colors } = require('tailwindcss/defaultTheme')
-
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        indiGogo: '#818cf8',
-        spaceBlue: '#60a5fa',
-        grassGreen: '#34d399',
-        strokeGray: 'rgb(112, 112, 112)',
-        grassGreen: '#b45309',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: { display: ['responsive', 'group-hover', 'group-focus'] },
