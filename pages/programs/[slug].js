88274522a04e56c61ff686e182b37b6de8078602
Sam Klepper <samklepdev@gmail.com>
2021-11-30 14:43:15 -0600

    New data from CMS pulled through data lib & programs slug page styling.

--- pages/programs/[slug].js
+++ pages/programs/[slug].js
@@ -2,6 +2,9 @@ import Layout from '../../components/Layout'
 import { getProgram, getProgramsSlugs } from '../../lib/data'
 import { Disclosure } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
+import he from 'he'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
 
 const faqs = [
   {
@@ -44,6 +47,8 @@ export const getStaticPaths = async () => {
   const slugRes = await getProgramsSlugs()
   const slugs = slugRes.programs
 
+  console.log(slugs)
+
   return {
     paths: slugs.map((slug) => ({ params: { slug: slug.slug } })),
     fallback: false,
@@ -56,12 +61,14 @@ export const getStaticProps = async ({ params }) => {
   return {
     props: {
       program: program.programs[0],
+      coverage: await serialize(he.decode(program.programs[0].coverage)),
+      highlights: await serialize(he.decode(program.programs[0].highlights)),
     },
   }
 }
 
-export default function ProgramPage({ program }) {
-  // console.log(program)
+export default function ProgramPage({ program, coverage, highlights }) {
+  console.log(program)
   return (
     <Layout title={`GIG || ${program.title}`}>
       {/* Hero */}
@@ -88,51 +95,39 @@ export default function ProgramPage({ program }) {
       </div>
 
       {/* Main content */}
-      <div className="py-16 bg-gray-50 overflow-hidden">
+      <div className="pt-16 bg-gray-50 overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
-          <div className="text-base max-w-prose mx-auto lg:max-w-none">
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              {program.title}
-            </p>
+          <div className="flex flex-col justify-center items-center my-6">
+            <div className="text-base max-w-prose  lg:max-w-none">
+              <p className="mt-2 mb-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                {program.subHeading}
+              </p>
+            </div>
+            <div className="relative z-10 prose prose-xl  mx-auto lg:max-w-5xl lg:mx-0 mb-20">
+              <p className="text-lg ">{program.description}</p>
+            </div>
           </div>
-          <div className="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
-            <p className="text-lg text-gray-500">
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi
-              vel porttitor vitae ut. Amet vitae fames senectus vitae.
-            </p>
-          </div>
+
           <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
             <div className="relative z-10">
-              <div className="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
-                <p>
-                  Sollicitudin tristique eros erat odio sed vitae, consequat
-                  turpis elementum. Lorem nibh vel, eget pretium arcu vitae.
-                  Eros eu viverra donec ut volutpat donec laoreet quam urna.
+              <div className="prose prose-xl max-w-none">
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Coverage
                 </p>
-                <ul role="list">
-                  <li>Quis elit egestas venenatis mattis dignissim.</li>
-                  <li>
-                    Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-                  </li>
-                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-                </ul>
-                <p>
-                  Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                  tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                  semper. Pellentesque in venenatis vestibulum consectetur nibh
-                  id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi
-                  enim fermentum lacus in. Viverra.
-                </p>
+                {<MDXRemote {...coverage} />}
               </div>
             </div>
-            <div className="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
-              <blockquote className="relative bg-white rounded-lg shadow-lg">
+            <div className="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none md:pt-32">
+              <blockquote className="relative bg-gray-50 rounded-lg shadow-lg p-8">
+                <img
+                  src={program.image.url}
+                  alt={program.title}
+                  className="h-auto rounded"
+                />
                 <img
                   src={program.image2.url}
                   alt={program.title}
-                  className="h-auto rounded"
+                  className="h-auto rounded mt-8"
                 />
               </blockquote>
             </div>
@@ -143,39 +138,13 @@ export default function ProgramPage({ program }) {
       {/* FAQs */}
       <div className="bg-gray-50">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-          <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
-            <h2 className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              Frequently asked questions
+          <div className="max-w-3xl mx-auto ">
+            <h2 className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl my-4">
+              Highlights
             </h2>
-            <dl className="mt-6 space-y-6 divide-y divide-gray-200">
-              {faqs.map((faq) => (
-                <Disclosure as="div" key={faq.question} className="pt-6">
-                  {({ open }) => (
-                    <>
-                      <dt className="text-lg">
-                        <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
-                          <span className="font-medium text-gray-900">
-                            {faq.question}
-                          </span>
-                          <span className="ml-6 h-7 flex items-center">
-                            <ChevronDownIcon
-                              className={classNames(
-                                open ? '-rotate-180' : 'rotate-0',
-                                'h-6 w-6 transform'
-                              )}
-                              aria-hidden="true"
-                            />
-                          </span>
-                        </Disclosure.Button>
-                      </dt>
-                      <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                        <p className="text-base text-gray-500">{faq.answer}</p>
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-              ))}
-            </dl>
+            <div className="prose prose-xl max-w-none mt-2">
+              {<MDXRemote {...highlights} />}
+            </div>
           </div>
         </div>
       </div>
