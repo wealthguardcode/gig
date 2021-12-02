b308141c1a5ef54221c5f478eb7091929e8e135d
Sam Klepper <samklepdev@gmail.com>
2021-12-02 16:19:30 -0600

    Resources page clean up & footer year.

--- pages/resources/broker-resources.js
+++ pages/resources/broker-resources.js
@@ -44,13 +44,6 @@ export default function BrokerResourcesPage() {
           <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
             Broker Resources
           </h1>
-          {/* <p className="mt-6 text-xl text-blue-100 max-w-3xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-            ornare eleifend justo, hendrerit venenatis ligula posuere sed. Duis
-            ac odio eget turpis accumsan dignissim quis sollicitudin diam. Sed
-            dictum nibh vitae hendrerit imperdiet. Aenean tincidunt sed est at
-            hendrerit.
-          </p> */}
         </div>
       </div>
 
@@ -58,12 +51,6 @@ export default function BrokerResourcesPage() {
       <div className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
           <div className="text-center">
-            {/* <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
-              Pricing
-            </h2>
-            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-              Take control of your team.
-            </p> */}
             <p className="max-w-3xl mt-5 mx-auto text-xl text-gray-500">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
               ornare eleifend justo, hendrerit venenatis ligula posuere sed.
@@ -104,45 +91,6 @@ export default function BrokerResourcesPage() {
             </div>
           ))}
         </div>
-      </div>
-
-      {/* Programs */}
-      <div className="p-8 my-10 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-        {/* {programs.map((program) => (
-          <div
-            className="max-w-sm rounded overflow-hidden shadow-xl mx-auto hover:shadow-2xl hover:border-indigo-200"
-            key={program.slug}>
-            <Link href={`/programs/${program.slug}`} passHref>
-              <a>
-                <img
-                  className="w-full"
-                  src={program.image.url}
-                  alt={program.title}
-                />
-              </a>
-            </Link>
-            <div className="px-6 py-4 flex flex-col items-center">
-              <Link href={`/programs/${program.slug}`} passHref>
-                <a>
-                  <div className="font-bold text-xl mb-2 text-center">
-                    {program.title}
-                  </div>
-                </a>
-              </Link>
-              <div className="flex justify-center">
-                <Link href={`/programs/${program.slug}`} passHref>
-                  <a>
-                    <button
-                      type="button"
-                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      Learn more
-                    </button>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))} */}
       </div>
 
       {/* CTA section */}
