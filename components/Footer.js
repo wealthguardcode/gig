b8c06e0ef306539112d56df1e69f12649cab8135
Sam Klepper <samklepdev@gmail.com>
2021-12-09 14:23:41 -0600

    Programs page-- branding colors styling.

--- components/Footer.js
+++ components/Footer.js
@@ -72,40 +72,40 @@ const footerNavigation = {
 export default function Footer() {
   return (
     <footer
-      aria-labelledby="footer-heading"
-      className="bg-white dark:bg-gray-700 border-t border-gray-200 dark:border-gray-500">
-      <h2 id="footer-heading" className="sr-only">
+      aria-labelledby='footer-heading'
+      className='bg-white dark:bg-gray-700 border-t border-gray-200 dark:border-gray-500'>
+      <h2 id='footer-heading' className='sr-only'>
         Footer
       </h2>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="py-20">
-          <div className="grid grid-cols-1 md:grid-cols-12 md:grid-flow-col md:gap-x-8 md:gap-y-16 md:auto-rows-min">
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='py-20'>
+          <div className='grid grid-cols-1 md:grid-cols-12 md:grid-flow-col md:gap-x-8 md:gap-y-16 md:auto-rows-min'>
             {/* Image section */}
-            <div className="col-span-1 md:col-span-2 lg:row-start-1 lg:col-start-1">
-              <Link href="/">
+            <div className='col-span-1 md:col-span-2 lg:row-start-1 lg:col-start-1'>
+              <Link href='/'>
                 <a>
                   <img
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=red&shade=600"
-                    alt="Logo"
-                    className="h-8 w-auto"
+                    src='https://tailwindui.com/img/logos/workflow-mark.svg?color=red&shade=600'
+                    alt='Logo'
+                    className='h-8 w-auto'
                   />
                 </a>
               </Link>
             </div>
 
             {/* Sitemap sections */}
-            <div className="mt-10 col-span-6 grid grid-cols-2 gap-8 sm:grid-cols-3 md:mt-0 md:row-start-1 md:col-start-3 md:col-span-8 lg:col-start-2 lg:col-span-6">
-              <div className="grid grid-cols-1 gap-y-12 sm:col-span-2 sm:grid-cols-2 sm:gap-x-8">
+            <div className='mt-10 col-span-6 grid grid-cols-2 gap-8 sm:grid-cols-3 md:mt-0 md:row-start-1 md:col-start-3 md:col-span-8 lg:col-start-2 lg:col-span-6'>
+              <div className='grid grid-cols-1 gap-y-12 sm:col-span-2 sm:grid-cols-2 sm:gap-x-8'>
                 <div>
-                  <h3 className="text-base font-medium tracking-wider text-gray-900 dark:text-gray-300">
+                  <h3 className='text-base font-medium tracking-wider text-gray-900 dark:text-gray-300'>
                     Quick Links
                   </h3>
-                  <ul role="list" className="mt-6 space-y-6">
+                  <ul role='list' className='mt-6 space-y-6'>
                     {footerNavigation.links.map((item) => (
-                      <li key={item.name} className="text-sm">
+                      <li key={item.name} className='text-sm'>
                         <a
                           href={item.href}
-                          className="text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800">
+                          className='text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800'>
                           {item.name}
                         </a>
                       </li>
@@ -113,15 +113,15 @@ export default function Footer() {
                   </ul>
                 </div>
                 <div>
-                  <h3 className="text-base font-medium tracking-wider text-gray-900 dark:text-gray-300">
+                  <h3 className='text-base font-medium tracking-wider text-gray-900 dark:text-gray-300'>
                     Resources
                   </h3>
-                  <ul role="list" className="mt-6 space-y-6">
+                  <ul role='list' className='mt-6 space-y-6'>
                     {footerNavigation.resources.map((item) => (
-                      <li key={item.name} className="text-sm">
+                      <li key={item.name} className='text-sm'>
                         <a
                           href={item.href}
-                          className="text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800">
+                          className='text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800'>
                           {item.name}
                         </a>
                       </li>
@@ -130,15 +130,15 @@ export default function Footer() {
                 </div>
               </div>
               <div>
-                <h3 className="text-base font-medium tracking-wider text-gray-900 dark:text-gray-300">
+                <h3 className='text-base font-medium tracking-wider text-gray-900 dark:text-gray-300'>
                   Company
                 </h3>
-                <ul role="list" className="mt-6 space-y-6">
+                <ul role='list' className='mt-6 space-y-6'>
                   {footerNavigation.company.map((item) => (
-                    <li key={item.name} className="text-sm">
+                    <li key={item.name} className='text-sm'>
                       <a
                         href={item.href}
-                        className="text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800">
+                        className='text-gray-500 dark:text-gray-400 hover:font-bold hover:text-red-700 dark:hover:text-red-700 hover:bg-gray-100 dark:hover:bg-gray-800'>
                         {item.name}
                       </a>
                     </li>
@@ -148,29 +148,29 @@ export default function Footer() {
             </div>
 
             {/* Newsletter section */}
-            <div className="mt-12 md:mt-0 md:row-start-2 md:col-start-3 md:col-span-8 lg:row-start-1 lg:col-start-9 lg:col-span-4">
-              <h3 className="text-sm font-medium text-gray-900 dark:text-gray-300">
+            <div className='mt-12 md:mt-0 md:row-start-2 md:col-start-3 md:col-span-8 lg:row-start-1 lg:col-start-9 lg:col-span-4'>
+              <h3 className='text-sm font-medium text-gray-900 dark:text-gray-300'>
                 Sign up for our newsletter
               </h3>
-              <p className="mt-6 text-sm text-gray-500 dark:text-gray-400">
+              <p className='mt-6 text-sm text-gray-500 dark:text-gray-400'>
                 The latest info & updates, sent to your inbox weekly.
               </p>
-              <form className="mt-2 flex sm:max-w-md" onSubmit={sendEmail}>
-                <label htmlFor="email-address" className="sr-only">
+              <form className='mt-2 flex sm:max-w-md' onSubmit={sendEmail}>
+                <label htmlFor='email-address' className='sr-only'>
                   Email address
                 </label>
                 <input
-                  id="subscriber_email"
-                  name="subscriber_email"
-                  type="text"
-                  autoComplete="email"
+                  id='subscriber_email'
+                  name='subscriber_email'
+                  type='text'
+                  autoComplete='email'
                   required
-                  className="appearance-none min-w-0 w-full bg-white dark:bg-gray-300 border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:border-red-700 focus:ring-1 focus:ring-red-700"
+                  className='appearance-none min-w-0 w-full bg-white dark:bg-gray-50 border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:border-red-700 focus:ring-1 focus:ring-red-700'
                 />
-                <div className="ml-4 flex-shrink-0">
+                <div className='ml-4 flex-shrink-0'>
                   <button
-                    type="submit"
-                    className="w-full bg-red-700 border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700">
+                    type='submit'
+                    className='w-full bg-red-700 border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700'>
                     Sign up
                   </button>
                 </div>
@@ -179,8 +179,8 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="border-t border-gray-100 dark:border-gray-500 py-10 text-center">
-          <p className="text-sm text-gray-500 dark:text-gray-300">
+        <div className='border-t border-gray-100 dark:border-gray-500 py-10 text-center'>
+          <p className='text-sm text-gray-500 dark:text-gray-300'>
             &copy; {new Date().getFullYear()} WealthGuard Insurance Group, Inc.
             All rights reserved.
           </p>
--- pages/programs/index.js
+++ pages/programs/index.js
@@ -17,24 +17,24 @@ export const getStaticProps = async () => {
 export default function ProgramsPage({ programs }) {
   // console.log(programs)
   return (
-    <Layout title="WIG | Our Programs">
-      <div className="relative bg-blue-400">
-        <div className="absolute inset-0">
+    <Layout title='WIG | Our Programs'>
+      <div className='relative bg-blue-400'>
+        <div className='absolute inset-0'>
           <img
-            className="w-full h-full object-cover"
-            src="https://media.graphcms.com/output=format:jpg/resize=,height:800,fit:max/n8fvyRIWRuK9U3wCa2tT"
+            className='w-full h-full object-cover'
+            src='https://media.graphcms.com/output=format:jpg/resize=,height:800,fit:max/n8fvyRIWRuK9U3wCa2tT'
             alt="GoGuard's Programs"
           />
           <div
-            className="absolute inset-0 bg-blue-800 mix-blend-multiply"
-            aria-hidden="true"
+            className='absolute inset-0 bg-gray-400 mix-blend-multiply'
+            aria-hidden='true'
           />
         </div>
-        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-          <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
+        <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
+          <h1 className='text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl'>
             Our Programs
           </h1>
-          <p className="mt-6 text-xl text-blue-100 max-w-3xl">
+          <p className='mt-6 text-xl text-gray-100 max-w-3xl'>
             We have years of insurance experience helping carriers and brokers
             provide solutions to their clients. Experience the difference of
             GoGuard&apos;s newest Programs and Binding Authority division.
@@ -43,34 +43,34 @@ export default function ProgramsPage({ programs }) {
       </div>
 
       {/* Programs */}
-      <div className="p-8 my-10 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+      <div className='p-8 my-10 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3'>
         {programs.map((program) => (
           <div
-            className="max-w-sm rounded overflow-hidden shadow-xl mx-auto hover:shadow-2xl border hover:border-blue-300 dark:hover:border-blue-600 dark:bg-gray-900"
+            className='max-w-sm rounded overflow-hidden shadow-xl mx-auto hover:shadow-2xl border hover:border-red-400 dark:hover:border-red-600 dark:bg-gray-900'
             key={program.slug}>
             <Link href={`/programs/${program.slug}`} passHref>
               <a>
                 <img
-                  className="w-full"
+                  className='w-full'
                   src={program.image.url}
                   alt={program.title}
                 />
               </a>
             </Link>
-            <div className="px-6 py-4 flex flex-col items-center">
+            <div className='px-6 py-4 flex flex-col items-center'>
               <Link href={`/programs/${program.slug}`} passHref>
                 <a>
-                  <div className="font-bold text-xl mb-2 text-center dark:text-gray-300">
+                  <div className='font-bold text-xl mb-2 text-center dark:text-gray-300'>
                     {program.title}
                   </div>
                 </a>
               </Link>
-              <div className="flex justify-center">
+              <div className='flex justify-center'>
                 <Link href={`/programs/${program.slug}`} passHref>
                   <a>
                     <button
-                      type="button"
-                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-blue-700 bg-blue-100 dark:bg-blue-300 dark:text-blue-800 hover:bg-blue-200 dark:hover:bg-blue-300 dark:hover:text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      type='button'
+                      className='inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-red-500 dark:bg-red-700 dark:text-white hover:bg-red-700 dark:hover:bg-red-500 dark:hover:text-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'>
                       Learn more
                     </button>
                   </a>
@@ -82,21 +82,21 @@ export default function ProgramsPage({ programs }) {
       </div>
 
       {/* CTA section */}
-      <div className="bg-blue-700">
-        <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-            <span className="block">Ready to learn more?</span>
+      <div className='bg-red-700'>
+        <div className='max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8'>
+          <h2 className='text-3xl font-extrabold text-white sm:text-4xl'>
+            <span className='block'>Ready to learn more?</span>
           </h2>
-          <p className="mt-4 text-lg leading-6 text-blue-200">
+          <p className='mt-4 text-lg leading-6 text-gray-200'>
             Do not hesitate to reach out to us with your questions or concerns
             today.
           </p>
-          <Link href="/contact-us">
-            <a className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto">
+          <Link href='/contact-us'>
+            <a className='mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-red-600 bg-white hover:bg-red-50 sm:w-auto'>
               Contact us{' '}
               <MailIcon
-                className="ml-3 h-5 w-5 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
+                className='ml-3 h-5 w-5 flex-shrink-0 text-gray-400'
+                aria-hidden='true'
               />
             </a>
           </Link>
