a15ee32df67f62bf20123a7dc6d02a08aca2b7fc
Sam Klepper <samklepdev@gmail.com>
2021-12-09 15:33:10 -0600

    Contact us page-- colors changed to match WealthGuard branding.

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -59,15 +59,15 @@ function sendEmail(e) {
 
 export default function ContactUsPage() {
   return (
-    <Layout title="WIG | Contact Us">
+    <Layout title='WIG | Contact Us'>
       <main>
         {/* Header */}
-        <div className="py-24 bg-gray-50 dark:bg-gray-900 sm:py-32">
-          <div className="max-w-md mx-auto pl-4 pr-8 sm:max-w-lg sm:px-6 lg:max-w-7xl lg:px-8">
-            <h1 className="text-4xl leading-10 font-extrabold tracking-tight text-gray-900 dark:text-gray-300 text-center sm:text-5xl sm:leading-none lg:text-6xl">
+        <div className='py-24 bg-gray-50 dark:bg-gray-900 sm:py-32'>
+          <div className='max-w-md mx-auto pl-4 pr-8 sm:max-w-lg sm:px-6 lg:max-w-7xl lg:px-8'>
+            <h1 className='text-4xl leading-10 font-extrabold tracking-tight text-gray-900 dark:text-gray-300 text-center sm:text-5xl sm:leading-none lg:text-6xl'>
               Contact Us
             </h1>
-            <p className="mt-6 max-w-3xl mx-auto text-xl leading-normal text-gray-500 dark:text-gray-400 text-center">
+            <p className='mt-6 max-w-3xl mx-auto text-xl leading-normal text-gray-500 dark:text-gray-400 text-center'>
               Convallis feugiat et aliquet pellentesque dictum nisi, velit.
               Egestas fermentum adipiscing risus quam ac consectetur mattis
               turpis tristique.
@@ -76,149 +76,149 @@ export default function ContactUsPage() {
         </div>
 
         {/* Contact Section */}
-        <div className="relative bg-white dark:bg-gray-800">
-          <div className="lg:absolute lg:inset-0">
-            <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div className='relative bg-white dark:bg-gray-800'>
+          <div className='lg:absolute lg:inset-0'>
+            <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
               <img
-                className="h-56 w-full object-cover lg:absolute lg:h-full"
-                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
-                alt=""
+                className='h-56 w-full object-cover lg:absolute lg:h-full'
+                src='https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+                alt=''
               />
             </div>
           </div>
-          <div className="relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2">
-            <div className="lg:pr-8">
-              <div className="max-w-md mx-auto sm:max-w-lg lg:mx-0">
-                <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl dark:text-gray-300">
+          <div className='relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2'>
+            <div className='lg:pr-8'>
+              <div className='max-w-md mx-auto sm:max-w-lg lg:mx-0'>
+                <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl dark:text-gray-300'>
                   Let&apos;s work together
                 </h2>
-                <p className="mt-4 text-lg text-gray-500 sm:mt-3 dark:text-gray-400">
+                <p className='mt-4 text-lg text-gray-500 sm:mt-3 dark:text-gray-400'>
                   We&apos;d love to hear from you! Send us a message using the
                   form opposite, or email us. We’d love to hear from you! Send
                   us a message using the form opposite, or email us.
                 </p>
                 <form
-                  id="template_tsp834s"
-                  method="POST"
-                  className="mt-9 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+                  id='template_tsp834s'
+                  method='POST'
+                  className='mt-9 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'
                   onSubmit={sendEmail}>
                   <div>
                     <label
-                      htmlFor="first-name"
-                      className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                      htmlFor='first-name'
+                      className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                       First name
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        type="text"
-                        name="first_name"
-                        id="first_name"
-                        autoComplete="given-name"
-                        className="block w-full shadow-sm sm:text-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                        type='text'
+                        name='first_name'
+                        id='first_name'
+                        autoComplete='given-name'
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md'
                       />
                     </div>
                   </div>
                   <div>
                     <label
-                      htmlFor="last-name"
-                      className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                      htmlFor='last-name'
+                      className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                       Last name
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        type="text"
-                        name="last_name"
-                        id="last_name"
-                        autoComplete="family-name"
-                        className="block w-full shadow-sm sm:text-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                        type='text'
+                        name='last_name'
+                        id='last_name'
+                        autoComplete='family-name'
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md'
                       />
                     </div>
                   </div>
-                  <div className="sm:col-span-2">
+                  <div className='sm:col-span-2'>
                     <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                      htmlFor='email'
+                      className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                       Email
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full shadow-sm sm:text-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
+                        id='email'
+                        name='email'
+                        type='email'
+                        autoComplete='email'
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md'
                       />
                     </div>
                   </div>
-                  <div className="sm:col-span-2">
+                  <div className='sm:col-span-2'>
                     <label
-                      htmlFor="company"
-                      className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                      htmlFor='company'
+                      className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                       Company
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        type="text"
-                        name="company"
-                        id="company"
-                        autoComplete="organization"
-                        className="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                        type='text'
+                        name='company'
+                        id='company'
+                        autoComplete='organization'
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md'
                       />
                     </div>
                   </div>
-                  <div className="sm:col-span-2">
-                    <div className="flex justify-between">
+                  <div className='sm:col-span-2'>
+                    <div className='flex justify-between'>
                       <label
-                        htmlFor="phone"
-                        className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                        htmlFor='phone'
+                        className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                         Phone
                       </label>
                       <span
-                        id="phone-description"
-                        className="text-sm text-gray-500 dark:text-gray-500">
+                        id='phone-description'
+                        className='text-sm text-gray-500 dark:text-gray-500'>
                         Optional
                       </span>
                     </div>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        type="text"
-                        name="phone"
-                        id="phone"
-                        autoComplete="tel"
-                        aria-describedby="phone-description"
-                        className="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                        type='text'
+                        name='phone'
+                        id='phone'
+                        autoComplete='tel'
+                        aria-describedby='phone-description'
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md'
                       />
                     </div>
                   </div>
-                  <div className="sm:col-span-2">
-                    <div className="flex justify-between">
+                  <div className='sm:col-span-2'>
+                    <div className='flex justify-between'>
                       <label
-                        htmlFor="how-can-we-help"
-                        className="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                        htmlFor='how-can-we-help'
+                        className='block text-sm font-medium text-gray-700 dark:text-gray-400'>
                         How can we help you?
                       </label>
                       <span
-                        id="how-can-we-help-description"
-                        className="text-sm text-gray-500">
+                        id='how-can-we-help-description'
+                        className='text-sm text-gray-500'>
                         Max. 500 characters
                       </span>
                     </div>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <textarea
-                        id="message"
-                        name="message"
-                        aria-describedby="how-can-we-help-description"
+                        id='message'
+                        name='message'
+                        aria-describedby='how-can-we-help-description'
                         rows={4}
-                        className="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border border-gray-300 rounded-md"
+                        className='block w-full shadow-sm sm:text-sm focus:ring-red-500 focus:border-red-500 border border-gray-300 rounded-md'
                         defaultValue={''}
                       />
                     </div>
                   </div>
 
-                  <div className="text-right sm:col-span-2">
+                  <div className='text-right sm:col-span-2'>
                     <button
-                      type="submit"
-                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      type='submit'
+                      className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'>
                       Submit
                     </button>
                   </div>
@@ -229,52 +229,52 @@ export default function ContactUsPage() {
         </div>
 
         {/* Contact Information */}
-        <div className="bg-white dark:bg-gray-800">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
-            <div className="divide-y-2 divide-gray-200">
-              <div className="grid md:grid-cols-2 lg:gap-8">
-                <h2 className="text-2xl font-extrabold text-gray-900 dark:text-gray-300 sm:text-3xl">
+        <div className='bg-white dark:bg-gray-800'>
+          <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8'>
+            <div className='divide-y-2 divide-gray-200'>
+              <div className='grid md:grid-cols-2 lg:gap-8'>
+                <h2 className='text-2xl font-extrabold text-gray-900 dark:text-gray-300 sm:text-3xl'>
                   Get in touch
                 </h2>
-                <div className="mt-8 1 gap-12 sm:gap-x-8 sm:gap-y-12 lg:mt-0 ">
+                <div className='mt-8 1 gap-12 sm:gap-x-8 sm:gap-y-12 lg:mt-0 '>
                   <div>
-                    <h3 className="text-2xl leading-6 tracking-wider font-medium text-gray-900 dark:text-gray-300">
+                    <h3 className='text-2xl leading-6 tracking-wider font-medium text-gray-900 dark:text-gray-300'>
                       Office
                     </h3>
-                    <dl className="mt-2 text-lg text-gray-500 dark:text-gray-400">
+                    <dl className='mt-2 text-lg text-gray-500 dark:text-gray-400'>
                       <div>
-                        <dt className="sr-only">Address</dt>
+                        <dt className='sr-only'>Address</dt>
                         <dd>7205 FM 1488 Rd, Ste-A</dd>
                         <dd>Magnolia, TX 77354</dd>
                       </div>
                       <div>
-                        <dt className="sr-only">Email</dt>
-                        <dd className="inline-flex items-center group">
+                        <dt className='sr-only'>Email</dt>
+                        <dd className='inline-flex items-center group'>
                           <MailIcon
                             height={20}
-                            className="mr-1 group-hover:text-blue-600 dark:group-hover:text-blue-600"
+                            className='mr-1 group-hover:text-red-600 dark:group-hover:text-red-600'
                           />
                           <a
-                            className="hover:text-blue-600 dark:hover:text-blue-600"
-                            href="mailto:sales@wealthguardig.com"
-                            target="_blank"
-                            rel="noreferrer">
+                            className='hover:text-red-600 dark:hover:text-red-600'
+                            href='mailto:sales@wealthguardig.com'
+                            target='_blank'
+                            rel='noreferrer'>
                             sales@wealthguardig.com
                           </a>
                         </dd>
                       </div>
-                      <div className="mt-1">
-                        <dt className="sr-only">Phone number</dt>
-                        <dd className="inline-flex items-center group">
+                      <div className='mt-1'>
+                        <dt className='sr-only'>Phone number</dt>
+                        <dd className='inline-flex items-center group'>
                           <PhoneIcon
                             height={20}
-                            className="mr-1 group-hover:text-blue-600 dark:group-hover:text-blue-600"
+                            className='mr-1 group-hover:text-red-600 dark:group-hover:text-red-600'
                           />
                           <a
-                            className="hover:text-blue-600 dark:hover:text-blue-600"
-                            href="tel:832-930-3827"
-                            target="_blank"
-                            rel="noreferrer">
+                            className='hover:text-red-600 dark:hover:text-red-600'
+                            href='tel:832-930-3827'
+                            target='_blank'
+                            rel='noreferrer'>
                             832-930-3827
                           </a>
                         </dd>
@@ -288,19 +288,21 @@ export default function ContactUsPage() {
         </div>
 
         {/* CTA Section */}
-        <div className="bg-gradient-to-r from-blue-400 via-indigo-500 to-blue-500">
-          <div className="max-w-md mx-auto text-center py-16 px-4 sm:max-w-2xl sm:py-24 sm:px-6 lg:px-8 lg:py-32">
-            <h2 className="text-3xl font-extrabold sm:text-4xl">
-              <span className="block text-white">Need something else?</span>
-              <span className="block text-blue-900">Drop us a line.</span>
+        <div className='bg-gradient-to-r from-red-500 via-red-400 to-red-500'>
+          <div className='max-w-md mx-auto text-center py-16 px-4 sm:max-w-2xl sm:py-24 sm:px-6 lg:px-8 lg:py-32'>
+            <h2 className='text-3xl font-extrabold sm:text-4xl'>
+              <span className='block text-white'>Need something else?</span>
+              <span className='block text-gray-700'>Drop us a line.</span>
             </h2>
             <a
-              href="#"
-              className="mt-8 w-full inline-flex items-center justify-center py-3 px-5 bg-white border border-transparent rounded-md shadow-md text-base font-medium text-blue-600 hover:bg-blue-50 sm:w-auto">
+              href='mailto:sales@wealthguardig.com'
+              target='_blank'
+              rel='noreferrer'
+              className='mt-8 w-full inline-flex items-center justify-center py-3 px-5 bg-white border border-transparent rounded-md shadow-md text-base font-medium text-red-600 hover:bg-red-50 sm:w-auto'>
               <span>Email Us</span>
               <MailIcon
-                className="ml-3 h-5 w-5 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
+                className='ml-3 h-5 w-5 flex-shrink-0 text-gray-400'
+                aria-hidden='true'
               />
             </a>
           </div>
