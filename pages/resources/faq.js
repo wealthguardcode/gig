5eac93fc3bc2a5abc58e352f76ca589e89a27062
Sam Klepper <samklepdev@gmail.com>
2021-12-09 15:13:41 -0600

    FAQ page-- colors changed to match WealthGuard branding.

--- pages/resources/faq.js
+++ pages/resources/faq.js
@@ -43,23 +43,23 @@ const faqs = [
 export default function FaqPage() {
   return (
     <Layout title="WIG | Faq's">
-      <div className="relative bg-blue-400">
-        <div className="absolute inset-0">
+      <div className='relative bg-red-400'>
+        <div className='absolute inset-0'>
           <img
-            className="w-full h-full object-cover"
-            src="https://media.graphcms.com/output=format:jpg/resize=,height:800,fit:max/oxQftSMxRRmZZAuhsuvK"
+            className='w-full h-full object-cover'
+            src='https://media.graphcms.com/output=format:jpg/resize=,height:800,fit:max/oxQftSMxRRmZZAuhsuvK'
             alt="Faq's"
           />
           <div
-            className="absolute inset-0 bg-blue-800 mix-blend-multiply"
-            aria-hidden="true"
+            className='absolute inset-0 bg-gray-400 mix-blend-multiply'
+            aria-hidden='true'
           />
         </div>
-        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-          <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
+        <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
+          <h1 className='text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl'>
             FAQ&apos;s
           </h1>
-          <p className="mt-6 text-2xl text-blue-100 max-w-3xl">
+          <p className='mt-6 text-2xl text-red-100 max-w-3xl'>
             Got questions? We&apos;ve got answers. Take a look at some of our
             most commonly asked questions.
           </p>
@@ -68,34 +68,36 @@ export default function FaqPage() {
 
       {/* FAQ section */}
       <section
-        aria-labelledby="faq-heading"
-        className="bg-white dark:bg-gray-800">
-        <div className="max-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="max-w-xl">
+        aria-labelledby='faq-heading'
+        className='bg-white dark:bg-gray-800'>
+        <div className='max-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+          <div className='max-w-xl'>
             <h2
-              id="faq-heading"
-              className="text-2xl font-extrabold tracking-wide text-gray-900 dark:text-gray-300">
+              id='faq-heading'
+              className='text-2xl font-extrabold tracking-wide text-gray-900 dark:text-gray-300'>
               Frequently asked questions
             </h2>
-            <p className="mt-4 text-lg text-gray-500 dark:text-gray-400">
+            <p className='mt-4 text-lg text-gray-500 dark:text-gray-400'>
               Take a look at our FAQ and if you still cannot find what you are
               looking for you can always{' '}
               <a
-                href="#"
-                className="font-medium text-blue-600 hover:text-blue-500">
+                href='mailto:sales@wealthguardig.com'
+                target='_blank'
+                rel='noreferrer'
+                className='font-medium text-red-600 hover:text-red-500'>
                 send us an email
               </a>{' '}
               with your enquiry.
             </p>
           </div>
 
-          <dl className="mt-12 grid grid-cols-1 gap-y-10 sm:mt-16 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3">
+          <dl className='mt-12 grid grid-cols-1 gap-y-10 sm:mt-16 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3'>
             {faqs.map((faq) => (
               <div key={faq.id}>
-                <dt className="text-base font-medium text-gray-900 dark:text-gray-300">
+                <dt className='text-base font-medium text-gray-900 dark:text-gray-300'>
                   {faq.question}
                 </dt>
-                <dd className="mt-3 text-sm text-gray-500 dark:text-gray-400">
+                <dd className='mt-3 text-sm text-gray-500 dark:text-gray-400'>
                   {faq.answer}
                 </dd>
               </div>
@@ -105,30 +107,25 @@ export default function FaqPage() {
       </section>
 
       {/* Testimonial section */}
-      <div className="bg-white dark:bg-gray-800 py-16 lg:py-24">
-        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="relative py-24 px-8 bg-blue-500 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
-            <div className="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
+      <div className='bg-white dark:bg-gray-800 py-16 lg:py-24'>
+        <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <div className='relative py-24 px-8 bg-red-700 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8'>
+            <div className='absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply'>
               <img
-                src="https://images.unsplash.com/photo-1609902726285-00668009f004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
-                alt=""
-                className="w-full h-full object-cover"
+                src='https://images.unsplash.com/photo-1609902726285-00668009f004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+                alt=''
+                className='w-full h-full object-cover'
               />
             </div>
-            <div className="relative lg:col-span-1">
-              {/* <img
-                className="h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workcation-logo-white.svg"
-                alt=""
-              /> */}
-              <blockquote className="mt-6 text-white">
-                <p className="text-xl font-medium sm:text-2xl">
+            <div className='relative lg:col-span-1'>
+              <blockquote className='mt-6 text-white'>
+                <p className='text-xl font-medium sm:text-2xl'>
                   WealthGuard has completely transformed how we interact with
                   brokers. Proin pulvinar, quam a porta consequat, mi lorem
                   mattis diam, in laoreet massa neque sit amet turpis.
                 </p>
-                <footer className="mt-6">
-                  <p className="flex flex-col font-medium">
+                <footer className='mt-6'>
+                  <p className='flex flex-col font-medium'>
                     <span>Jane Doe</span>
                     <span>CEO, Example Inc.</span>
                   </p>
