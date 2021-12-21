69bbfa89d43eb095340b9dea5e37809700c9bc6b
Sam Klepper <samklepdev@gmail.com>
2021-12-21 11:29:20 -0600

    Progrmas page-- preconnecting to Google Fonts API.

--- components/Layout.js
+++ components/Layout.js
@@ -5,15 +5,17 @@ import ScrollToTopButton from './ScrollToTopButton'
 
 export default function Layout({ title, keywords, description, children }) {
   return (
-    <div className="bg-white dark:bg-gray-800">
+    <div className='bg-white dark:bg-gray-800'>
       <Head>
         <title>{title}</title>
-        <meta name="keywords" content={keywords} />
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+          rel='stylesheet'
         />
       </Head>
 
