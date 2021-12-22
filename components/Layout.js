21fb8089edfd501fec83e27ff33267387ea19824
Sam Klepper <samklepdev@gmail.com>
2021-12-22 11:59:07 -0600

    Font opt test.

--- components/Layout.js
+++ components/Layout.js
@@ -11,12 +11,12 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='keywords' content={keywords} />
         <meta name='description' content={description} />
         <link rel='icon' href='/favicon.ico' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
           href='https://fonts.gstatic.com'
           crossOrigin='true'
-        />
+        /> */}
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
           rel='stylesheet'
