478bf71fa6d243fa253f29cb1988fe1f69a8c44a
Sam Klepper <samklepdev@gmail.com>
2021-12-28 13:54:07 -0600

    Contact page image positioning & styling.

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -88,14 +88,9 @@ export default function ContactUsPage() {
         <div className='relative bg-white dark:bg-gray-800'>
           <div className='lg:absolute lg:inset-0'>
             <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
-              <Image
+              <img
                 className='h-56 w-full object-cover lg:absolute lg:h-full'
                 src='https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
-                width={940}
-                height={928}
-                priority={true}
-                placeholder='blur'
-                blurDataURL
                 alt='Modern office'
               />
             </div>
