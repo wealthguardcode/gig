af6bf819addaf93f71047ef69ae288471d086cc9
Sam Klepper <samklepdev@gmail.com>
2021-12-29 14:13:08 -0600

    Coastal property CMS data changes.

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import emailjs from 'emailjs-com'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-// Coastal
 
 function fun() {
   document.getElementById('subscriber_email').value = ''
