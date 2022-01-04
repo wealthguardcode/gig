12b1c3eed2890d765ac2817933c3ec6214467ce6
Sam Klepper <samklepdev@gmail.com>
2022-01-04 09:21:40 -0600

    Mobile menu links spacing.

--- components/Header.js
+++ components/Header.js
@@ -163,7 +163,7 @@ export default function Header() {
                       <Tab.Panel
                         key={category.name}
                         className='px-4 py-6 space-y-12'>
-                        <div className='grid grid-cols-1 gap-x-4 gap-y-8'>
+                        <div className='grid grid-cols-1 gap-x-4 gap-y-4'>
                           {category.programs.map((item) => (
                             <div
                               key={item.name}
