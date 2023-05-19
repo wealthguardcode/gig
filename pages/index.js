871bbae9a541a7c0ad60ede908f55524cad5bc47
wealthguardcode <102389728+wealthguardcode@users.noreply.github.com>
2023-05-19 15:50:49 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -240,8 +240,8 @@ export default function Home() {
               className="mediaobject"
               src="/images/Coverholder at Lloyds_black_rgb.png"
               alt="Coverholder at Lloyds logo black"
-              width={auto}
-              height={auto}
+              width={400}
+              height={150}
              />
             <p className="mt-4 max-w-2xl prose-2xl md:tracking-wide text-gray-500 dark:text-gray-400 mx-auto">
               WealthGuard Insurance Group is a Coverholder at Lloyd&apos;s.
