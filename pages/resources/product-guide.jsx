d0889f4df821d8af9718b9e8c3ff9161d4592b8d
wealthguardcode <102389728+wealthguardcode@users.noreply.github.com>
2023-07-11 19:28:03 -0500

    Update product-guide.jsx

--- pages/resources/product-guide.jsx
+++ pages/resources/product-guide.jsx
@@ -18,7 +18,7 @@ export default function BrokerResourcesPage() {
     <Layout title='WIG | Product Guide'>
       <Hero
         imageUrl='https://images.unsplash.com/photo-1501290836517-b22a21c522a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
-        title='Broker Resources'
+        title='Product Guide'
       />
 
       {/* Main */}
