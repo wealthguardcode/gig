665da187ff077492f24aea2630bf6a510c636e18
wealthguardcode <102389728+wealthguardcode@users.noreply.github.com>
2023-08-30 13:17:12 -0500

    Update TrackDownloadList.jsx

--- components/TrackDownloadList.jsx
+++ components/TrackDownloadList.jsx
@@ -15,6 +15,18 @@ export default function TrackDownloadList({ items }) {
   )
 }
 
+export default function TrackDownloadList({ items }) {
+  return (
+    <ul className='not-prose list-inside list-none flex flex-col gap-2 -ml-4 text-base md:text-lg'>
+      {items.map((item) => (
+        <li key={item.title} className='group flex gap-4 items-start'>
+          <DocumentArrowDownIcon className='flex-none w-6 h-6 mt-0.5 text-gray-600 group-hover:text-gray-900 dark:text-gray-500 dark:group-hover:text-gray-400' />
+          <TrackDownloadLink href={item.href}>{item.title}</TrackDownloadLink>
+        </li>
+      ))}
+    </ul>
+  )
+
 function TrackDownloadLink({ href, children }) {
   const trackDownload = (url, event) => {
     gtag.event('file_download', {
