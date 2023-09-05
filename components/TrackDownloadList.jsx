1c9f44881f9e375f40d98f513bb4d9f82f8f9f8a
wealthguardcode <102389728+wealthguardcode@users.noreply.github.com>
2023-09-05 16:21:26 -0500

    Update TrackDownloadList.jsx

--- components/TrackDownloadList.jsx
+++ components/TrackDownloadList.jsx
@@ -22,6 +22,11 @@ function TrackDownloadLink({ href, children }) {
       event_label: children,
       file_path: url,
     })
+    gtag.event('file_download', {
+      event_category: 'Inland Marine PDF Download',
+      event_label: children,
+      file_path: url,
+    })
     
     window.open(url)
   }
